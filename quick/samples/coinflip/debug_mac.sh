1bd6b0e27525d71c3b2df3209d0a4df3caa37f1b
SunLightJuly <ygqy8181@qq.com>
2015-03-23 15:58:16 +0800

    generate simulator of mac to runtime dir

--- quick/samples/coinflip/debug_mac.sh
+++ quick/samples/coinflip/debug_mac.sh
@@ -9,7 +9,7 @@ if [ ! -d $QUICK_FRAME_PATH/cocos ]; then
     cp -rf $COCOS_SCRIPT_PATH $QUICK_FRAME_PATH/cocos
 fi
 
-BIN="$QUICK_V3_ROOT/Simulator.app/Contents/MacOS/Simulator"
+BIN="$QUICK_V3_ROOT/tools/simulator/runtime/mac/Simulator.app/Contents/MacOS/Simulator"
 if [ ! -f $BIN ]; then
     echo "PLEASE BUILD $QUICK_V3_ROOT/tools/simulator/frameworks/runtime-src/proj.ios_mac/simulator.xcodeproj FIRST"
     exit
