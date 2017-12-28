5fcfb800a58ebb049cea5fa9f6149f46b9e242b8
wshao-air <w.shao.fei@hotmail.com>
2017-12-28 16:46:02 +0800

    luaide 配置

--- quick/templates/shared/src/main.lua
+++ quick/templates/shared/src/main.lua
@@ -1,11 +1,15 @@
+package.path = package.path .. ";src/"
+
+local breakSocketHandle, debugXpCall = require("LuaDebug")("localhost", 7003)
+cc.Director:getInstance():getScheduler():scheduleScriptFunc(breakSocketHandle, 0.3, false)
 
 function __G__TRACKBACK__(errorMessage)
     print("----------------------------------------")
     print("LUA ERROR: " .. tostring(errorMessage) .. "\n")
     print(debug.traceback("", 2))
     print("----------------------------------------")
+    debugXpCall()
 end
 
-package.path = package.path .. ";src/"
 cc.FileUtils:getInstance():setPopupNotify(false)
 require("app.MyApp").new():run()
